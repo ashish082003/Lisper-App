658f03cb41cd0f69fa44d34a5b9e6ecb52daf5c4
ashish082003 <aj9172243856@gmail.com>
2023-08-02 09:40:54 +0530

    initial

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import axios from "axios";
+
+const port=3000;
+const app=express();
+
+app.use(express.static("public"));
+app.get("/", async(req,res)=>{
+    try{
+        const result=await axios.get("https://secrets-api.appbrewery.com/random");
+        res.render("index.ejs",{
+            secret: result.data.secret,
+            user: result.data.username,
+        });
+    }catch(error){
+        console.log(error.response.data);
+        res.status(500);
+    }
+});
+
+app.listen(port,()=>{
+    console.log(`Server is listed on port no ${port}`)
+});
